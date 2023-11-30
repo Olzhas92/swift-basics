598b6684f0db55c2dc3a5a3fb8ba524a01af5b3b
olzhaskhamzin <olzhas.khamzin@gmail.com>
2023-11-30 20:42:32 +0600

    end of Lab 1

--- swift-basics.playground/Contents.swift
+++ swift-basics.playground/Contents.swift
@@ -14,4 +14,29 @@ var numberOfContactsToDelete: Int = 5
 print("The number of my friends is \(friends - numberOfContactsToDelete)")
 
 // Lab 1. Task 3
+let dailyNumberOfStep: Int = 10000
+print("I need to achieve \(dailyNumberOfStep) steps daily.")
 
+// Lab 1. Task 4
+var schooling: Int = 11
+var extraYear: Int = 1
+var totalNumberOfStudyYears: Int = schooling + extraYear
+schooling = totalNumberOfStudyYears
+print("I studied \(schooling) years in school.")
+print("But then I studied extra year. So the total number of studying was \(schooling) years.")
+
+// Lab 1. Task 5
+var steps = 0
+print("Start count of steps is \(steps).")
+steps = 2000
+print("Throughout the day I've made \(steps) steps.")
+
+// Lab 1. Task 6
+var name: String?
+name = "Olzhas"
+print("My name is \(name!).")
+
+// Lab 1. Task 7
+var distanceTravelled: Double = 0
+distanceTravelled = 54.3
+print("Total distance travelled equals to \(distanceTravelled).")
