5d1015a6235da4229d49142d2e0bc49c21eea417
olzhaskhamzin <olzhas.khamzin@gmail.com>
2023-11-30 20:31:34 +0600

    start of lab works

---
+++ swift-basics.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// Swift Basics
+// Lab 1. Task 1
+print("Hello World!")
+print("I study swift.")
+print("I like programming!")
+
+// Lab 1. Task 2
+let friends: Int = 100
+var numberOfContactsToDelete: Int = 5
+print("The number of my friends is \(friends - numberOfContactsToDelete)")
+
+// Lab 1. Task 3
+
