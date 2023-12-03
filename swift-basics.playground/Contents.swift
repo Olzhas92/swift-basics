5e8a4d5927f09dcc0d89981ffbd19ac4be5034e7
olzhaskhamzin <olzhas.khamzin@gmail.com>
2023-12-03 21:19:00 +0600

    Lab 2.1 is complete

--- swift-basics.playground/Contents.swift
+++ swift-basics.playground/Contents.swift
@@ -4,46 +4,53 @@ var greeting = "Hello, playground"
 
 // Swift Basics
 // Lab 1. Task 1
-print("Hello World!")
+/*print("Hello World!")
 print("I study swift.")
 print("I like programming!")
+ */
 
 // Lab 1. Task 2
-let friends: Int = 100
+/*let friends: Int = 100
 var numberOfContactsToDelete: Int = 5
 print("The number of my friends is \(friends - numberOfContactsToDelete)")
+ */
 
 // Lab 1. Task 3
-let dailyNumberOfStep: Int = 10000
+/*let dailyNumberOfStep: Int = 10000
 print("I need to achieve \(dailyNumberOfStep) steps daily.")
+ */
 
 // Lab 1. Task 4
-var schooling: Int = 11
+/*var schooling: Int = 11
 var extraYear: Int = 1
 var totalNumberOfStudyYears: Int = schooling + extraYear
 schooling = totalNumberOfStudyYears
 print("I studied \(schooling) years in school.")
 print("But then I studied extra year. So the total number of studying was \(schooling) years.")
+*/
 
 // Lab 1. Task 5
-var steps = 0
+/*var steps = 0
 print("Start count of steps is \(steps).")
 steps = 2000
 print("Throughout the day I've made \(steps) steps.")
+*/
 
 // Lab 1. Task 6
-var name: String?
+/*var name: String?
 name = "Olzhas"
 print("My name is \(name!).")
+ */
 
 // Lab 1. Task 7
-var distanceTravelled: Double = 0
+/*var distanceTravelled: Double = 0
 distanceTravelled = 54.3
 print("Total distance travelled equals to \(distanceTravelled).")
+ */
 
 
 // Lab 2.1 Task 1
-let width = 10
+/*let width = 10
 let height = 20
 let area: Int?
 area = width * height
@@ -54,15 +61,17 @@ print("The room area equals to \(roomArea!).")
 let perimeter: Int?
 perimeter = width + width + height + height
 print("The perimeter equals to \(perimeter!).")
+ */
 
 // Lab 2.1 Task 2
-let dozen: Int = 12
+/*let dozen: Int = 12
 var remainingValue: Int?
 remainingValue = dozen % 5
 print("12 % 5 equals to \(remainingValue!)")
+ */
 
 // Lab 2.1 Task 3
-let heartRate1 = 60
+/*let heartRate1 = 60
 let heartRate2 = 80
 let heartRate3 = 99
 let addedHR: Int?
@@ -80,3 +89,37 @@ addedHRD = heartRate1D + heartRate2D + heartRate3D
 print("Added HRD equals to \(addedHRD!)")
 averageHRD = addedHRD! / 3
 print("Average HRD equals to \(averageHRD!)")
+ */
+
+// Lab 2.1 Task 4
+/*let steps: Double = 3467
+ let goal: Double = 10000
+ let percentOfGoal: Double?
+ percentOfGoal = (steps / goal) * 100
+ print("3467 of 10000 percentagewise equals to \(percentOfGoal!)")
+ */
+
+// Lab 2.1 Task 5
+/*var balance = 0
+ balance += 10000
+ print("Lawn mowing brought 10000 KZT. Total: \(balance) KZT.")
+ balance += 20000
+ print("Freelance works added extra 20000 KZT. Total: \(balance) KZT.")
+ balance -= balance / 2
+ print("Spent half of balance for dinner and movie night. Total: \(balance) KZT.")
+ balance *= 3
+ print("Window cleaning was 3 times worth my then-current balance. Total: \(balance) KZT.")
+ balance -= 3000
+ print("Spent 3000 KZT doing shopping. Grand total: \(balance) KZT.")
+ */
+
+// Lab 2.1 Task 6
+/*print("10 + 2 * 5 equals to 20")
+ print("10 + 2 * 5 equals to \(10 + 2 * 5)")
+ print("(10 + 2) * 5 equals to 60")
+ print("(10 + 2) * 5 equals to \((10 + 2) * 5)")
+ print("4*9 - 6/2 equals to 33")
+ print("4*9 - 6/2 equals to \(4*9 - 6/2)")
+ print("4 * (9 - 6) / 2 equals to 6")
+ print("4 * (9 - 6) / 2 equals to \(4 * (9 - 6) / 2)")
+ */
